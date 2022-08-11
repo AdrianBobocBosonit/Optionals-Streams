5be50e915c3866eb11397c69da9e69b5129d5060
AdrianBobocBosonit <adrian.boboc@bosonit.com>
2022-08-11 12:12:56 +0200

    Main2 funcionando completamente segun los requisitos

--- src/main2.java
+++ src/main2.java
@@ -27,23 +27,22 @@ public class main2 {
 
         listaPersonas.stream().filter(personas -> personas.getEdad() < 25 )
                 .filter(personas -> personas.getEdad() != 0)
-                .map(personas -> Optional.ofNullable(personas.getNombre()).orElse("Unknown")+
-                        Optional.ofNullable(personas.getPoblacion()).orElse("Unknown")+
-                        Optional.ofNullable(personas.getEdad()).orElse(0))
+                .map(personas -> "Nombre: " + Optional.ofNullable(personas.getNombre()).orElse("Unknown")+
+                        " |Poblacion: " + Optional.ofNullable(personas.getPoblacion()).orElse("Unknown")+
+                        " |Edad: " + Optional.ofNullable(personas.getEdad()).orElse(0))
                 .forEach(System.out::println);
     }
 
     public static List<Persona> OpenFile() throws FileNotFoundException {
         List<Persona> peopleList = new ArrayList<>();
-        List<String> people = new ArrayList<>();
         File f = new File("Text1.txt");
         Scanner sc = new Scanner(f);
         while(sc.hasNextLine()){
             String s = sc.nextLine();
             String[] person = s.split(":");
             Persona p=  new Persona();
-            p.setNombre(person[0]);
-            p.setPoblacion(person[1]);
+            p.setNombre(person[0].isEmpty()?null:person[0]);
+            p.setPoblacion(person[1].isEmpty()?null:person[1]);
             p.setEdad(person.length>2?Integer.parseInt(person[2]):0);
             peopleList.add(p);
         }
